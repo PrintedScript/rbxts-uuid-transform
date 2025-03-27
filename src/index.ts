d653d750372108572c7c47c2862f3f06ae023d6f
PrintedScript <64908610+PrintedScript@users.noreply.github.com>
2025-03-27 22:05:00 +0800

    add verbose

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import ts, { factory } from "typescript";
 import {v4 as uuidv4} from 'uuid';
 
+interface TransformerConfiguration {
+    verbose: boolean;
+}
+
+const defaultConfiguration: TransformerConfiguration = {
+    verbose: false
+}
+
 function hasUUIDTag(statement: ts.EnumDeclaration) : boolean {
     const JSDocTags = ts.getJSDocTags(statement);
     for ( const tag of JSDocTags ) {
@@ -9,35 +17,39 @@ function hasUUIDTag(statement: ts.EnumDeclaration) : boolean {
     return false;
 }
 
-function transformStatement( statement: ts.Statement ) : ts.Statement {
+function transformStatement( statement: ts.Statement, configuration: TransformerConfiguration ) : ts.Statement {
     if ( 
         !ts.isEnumDeclaration(statement) ||
         !(statement.modifiers?.find((modifier) => modifier.kind === ts.SyntaxKind.ConstKeyword))
     ) { return statement; }
     if ( !hasUUIDTag(statement) ) { return statement; }
 
+    if ( configuration.verbose ) { console.log(`Transforming ${statement.name.getText()}`); }
     return factory.updateEnumDeclaration(
         statement,
         statement.modifiers,
         statement.name,
         statement.members.map((member) => {
+            const generatedUUID = uuidv4();
+            if ( configuration.verbose ) { console.log(`${statement.name.getText()}.${member.name.getText()} -> ${generatedUUID}`); };
             return factory.updateEnumMember(
                 member,
                 member.name,
                 factory.createStringLiteral(
-                    uuidv4()
+                    generatedUUID
                 )
             )
         })
     );
 }
 
-export default function transform(program: ts.Program) {
+export default function transform(program: ts.Program, configuration: TransformerConfiguration = defaultConfiguration) {
+    configuration = { ...defaultConfiguration, ...configuration };
     return (ctx: ts.TransformationContext) => {
         return (sourceFile: ts.SourceFile) => {
             const newStatements = new Array<ts.Statement>();
             sourceFile.statements.forEach((statement) => {
-                const transformedStatement = transformStatement(statement);
+                const transformedStatement = transformStatement(statement, configuration);
                 newStatements.push(transformedStatement);
             });
             return factory.updateSourceFile(
